69642e053e8c5a41b36e9fef3da0726c9cde65b2
renato-milano <renatomilano96@libero.it>
2021-10-19 12:30:27 +0200

    modelComplexity=1 fix runtime error

--- detection.js
+++ detection.js
@@ -10,6 +10,7 @@ const hands = new Hands({locateFile: (file) => {
 
 hands.setOptions({
   maxNumHands: 4,
+  modelComplexity: 1,
   minDetectionConfidence: 0.8,
   minTrackingConfidence: 0.5
 });
@@ -18,7 +19,7 @@ hands.onResults(gotHands);
 
 function gotHands(results) {
   detections = results;
-  //console.log(detections);
+  console.log(detections);
   //console.log("DUCE")
 }
 
